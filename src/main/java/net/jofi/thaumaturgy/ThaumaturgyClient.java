719aac642ab6539be404626c7d60175261363164
thepotatanata <jofi.koe.9@gmail.com>
2026-06-10 20:56:58 +0100

    Initial commit

---
+++ src/main/java/net/jofi/thaumaturgy/ThaumaturgyClient.java
@@ -0,0 +1,10 @@
+package net.jofi.thaumaturgy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThaumaturgyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/jofi/thaumaturgy/ThaumaturgyDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jofi.thaumaturgy;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ThaumaturgyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
